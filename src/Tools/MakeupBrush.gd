07193ce0d66dbce91e7049304df02296b9b4a42d
test <82846064+TriLinder@users.noreply.github.com>
2024-01-26 23:57:46 +0100

    MakeupBrush now draws new point only when the distance from the last one is greater than specified value

--- src/Tools/MakeupBrush.gd
+++ src/Tools/MakeupBrush.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var brush_stroke_container : Node2D
+@export var point_distance_diff : float = 10
 
 @onready var BRUSH_STROKE : = preload("res://src/Tools/BrushStroke.tscn")
 
@@ -9,6 +10,7 @@ var brush_stroke : Line2D
 enum {IDLE, DRAWING}
 var brush_state = IDLE
 
+var last_point_pos : Vector2
 
 func _process(delta):
 	match brush_state:
@@ -17,9 +19,23 @@ func _process(delta):
 				brush_stroke = BRUSH_STROKE.instantiate()
 				brush_stroke_container.add_child(brush_stroke)
 				
+				brush_stroke.add_point(global_position)
+				brush_stroke.add_point(global_position + Vector2.RIGHT)
+				
+				last_point_pos = global_position
+				
 				brush_state = DRAWING
 		DRAWING:
-			brush_stroke.add_point(global_position)
+			if last_point_pos.distance_to(global_position) > point_distance_diff:
+				brush_stroke.add_point(global_position)
+				last_point_pos = global_position
+				
+			if Input.is_action_just_released("UseToolRight"):
+				brush_stroke.add_point(global_position)
+				brush_stroke.add_point(global_position + Vector2.RIGHT)
+
+				brush_state = IDLE
+				
+			
 	
-	if Input.is_action_just_released("UseToolRight"):
-		brush_state = IDLE
+	
