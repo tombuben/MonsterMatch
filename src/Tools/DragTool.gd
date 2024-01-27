3e828a2231224a14e7ed6928ce7aec7819590d8a
tombuben <tom+github@tombuben.eu>
2024-01-27 12:20:10 +0100

    merge conflicts

---
+++ src/Tools/DragTool.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var highlighted_item : RigidBody2D
+
+
+enum {IDLE, DRAGGING}
+var brush_state = IDLE
+
+@onready var last_position = global_position
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	match brush_state:
+		IDLE:
+			print(highlighted_item)
+			if Input.is_action_just_pressed("UseToolRight") and highlighted_item != null:
+				brush_state = DRAGGING
+		DRAGGING:
+			var translateDelta = global_position - last_position
+			highlighted_item.translate(translateDelta)
+			if Input.is_action_just_released("UseToolRight"):
+				brush_state = IDLE
+	
+	last_position = global_position;
+
+func _on_area_2d_body_entered(body):
+	highlighted_item = body
+
+
+func _on_area_2d_body_exited(body):
+	if brush_state != DRAGGING && body == highlighted_item:
+		highlighted_item = null
