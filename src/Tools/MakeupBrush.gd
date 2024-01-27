c327876f575b201cee034e60e1a96e1429842287
test <82846064+TriLinder@users.noreply.github.com>
2024-01-27 01:24:34 +0100

    Added Draw Area

--- src/Tools/MakeupBrush.gd
+++ src/Tools/MakeupBrush.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var brush_stroke_container : Node2D
 @export var point_distance_diff : float = 10
+@export var draw_area : Polygon2D
 
 @onready var BRUSH_STROKE : = preload("res://src/Tools/BrushStroke.tscn")
 
@@ -16,16 +17,23 @@ func _process(delta):
 	match brush_state:
 		IDLE:
 			if Input.is_action_just_pressed("UseToolRight"):
-				brush_stroke = BRUSH_STROKE.instantiate()
-				brush_stroke_container.add_child(brush_stroke)
+				if Geometry2D.is_point_in_polygon(global_position, draw_area.polygon):
+					brush_stroke = BRUSH_STROKE.instantiate()
+					brush_stroke_container.add_child(brush_stroke)
+					
+					brush_stroke.add_point(global_position)
+					brush_stroke.add_point(global_position + Vector2.RIGHT)
+					
+					last_point_pos = global_position
 				
-				brush_stroke.add_point(global_position)
-				brush_stroke.add_point(global_position + Vector2.RIGHT)
-				
-				last_point_pos = global_position
 				
 				brush_state = DRAWING
 		DRAWING:
+			if !Geometry2D.is_point_in_polygon(global_position, draw_area.polygon):
+				brush_stroke = BRUSH_STROKE.instantiate()
+				brush_stroke_container.add_child(brush_stroke)
+				return
+			
 			if last_point_pos.distance_to(global_position) > point_distance_diff:
 				brush_stroke.add_point(global_position)
 				last_point_pos = global_position
@@ -35,7 +43,4 @@ func _process(delta):
 				brush_stroke.add_point(global_position + Vector2.RIGHT)
 
 				brush_state = IDLE
-				
-			
-	
-	
+
