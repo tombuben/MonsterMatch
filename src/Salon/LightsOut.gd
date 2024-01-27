36b1a20125d5ffaa31d06db011eec0d6c6505797
tombuben <tom+github@tombuben.eu>
2024-01-27 21:02:28 +0100

    added light switch to salon

---
+++ src/Salon/LightsOut.gd
@@ -0,0 +1,23 @@
+extends ColorRect
+
+@onready var player = get_node("AnimationPlayer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	visible = false
+	
+func _process(_delta):
+	if Input.is_action_just_pressed("LightSwitchToggle"):
+		toggle()
+
+func turn_off_lights(flicker : bool = false):
+	if flicker == true:
+		player.play("LightsOut")
+	else:
+		visible = false
+
+func turn_on_lights(flicker : bool = false):
+	visible = false
+
+func toggle():
+	visible = !visible
