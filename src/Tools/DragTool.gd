6ce46782f5951225c9a25d836235d721c1e342aa
tombuben <tom+github@tombuben.eu>
2024-01-27 15:18:57 +0100

    fix barnabus

--- src/Tools/DragTool.gd
+++ src/Tools/DragTool.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var highlighted_item : Node2D
+var dragging_item : Node2D
 
 
 enum {IDLE, DRAGGING}
@@ -18,12 +19,14 @@ func _process(delta):
 	match brush_state:
 		IDLE:
 			if Input.is_action_just_pressed("UseToolRight") and highlighted_item != null:
+				dragging_item = highlighted_item
 				brush_state = DRAGGING
-		DRAGGING:
-			var translateDelta = global_position - last_position
-			highlighted_item.translate(translateDelta)
-			if Input.is_action_just_released("UseToolRight"):
+		DRAGGING:	
+			if Input.is_action_just_released("UseToolRight") or dragging_item == null:
 				brush_state = IDLE
+			else:
+				var translateDelta = global_position - last_position
+				dragging_item.translate(translateDelta)
 	
 	last_position = global_position;
 
@@ -32,7 +35,7 @@ func _on_area_2d_body_entered(body):
 
 
 func _on_area_2d_body_exited(body):
-	if brush_state != DRAGGING && body == highlighted_item:
+	if body == highlighted_item:
 		highlighted_item = null
 
 
@@ -41,5 +44,5 @@ func _on_area_2d_area_entered(area):
 
 
 func _on_area_2d_area_exited(area):
-	if brush_state != DRAGGING && area.get_parent() == highlighted_item:
+	if area.get_parent() == highlighted_item:
 		highlighted_item = null
